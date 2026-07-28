abce4ce3adbb7dc0dc95a2c01c35b814f0202d8f
James Dunstan <jdunstan355@gmail.com>
2026-07-28 18:15:01 +1000

    More Learning about Vue, Begining own pages

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -1,26 +1,61 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar
+        :pages="pages"
+        :active-page="activePage"
+        :nav-link-click="(index) => activePage = index"
+    ></navbar>
+
+   <!-- <page-viewer
+        v-if="pages.length > 0"
+        :page="pages[activePage]" 
+        ></page-viewer> -->
+
+    <create-page 
+        :page-created = "pageCreated"
+    ></create-page>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageViewer from './components/PageViewer.vue';
+import Navbar from './components/Navbar.vue';
+import CreatePage from './components/CreatePage.vue';
+
+
+
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
+    components: {
+        Navbar,
+        PageViewer,
+        CreatePage
+    },
+    created() {
+        this.getPages();
+    },
+    
+    
+    data(){
+                return {
+                   activePage: 0, 
+                   
+                   
+                   pages: this.getPages()
+                   
+                };
+            },
+            methods: {
+                async getPages() {
+                    let res = await fetch('pages.json');
+                    let data = await res.json();
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+                    this.pages = data;
+                },
+                pageCreated(pageObj){
+                    this.pages.push(pageObj);
+                }
+            }
 }
-</style>
+
+</script>
---
+++ vue-start-spa/src/components/CreatePage.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container mb-3">
+        <form action="">
+            <label for="" class="form-label">
+                Page Title
+            </label>
+            <input
+                type="text"
+                class="form-control"
+                v-model="pageTitle"
+
+            />
+            <div class="mb-3">
+                <label for="" class="form-label">
+                    Content
+                </label>
+                <textarea
+                    type="text"
+                    class="form-control"
+                    rows="5"
+                    v-model="content"
+                ></textarea>
+            </div>
+            <div class="mb-3">
+                <button
+                    class="btn btn-primary"
+                    :disabled="isFormInvalid"
+                    @click.prevent="submitForm"
+                >Create Page</button>
+            </div>
+        </form>
+    </div>
+
+
+
+
+
+
+
+</template>
+
+
+<script>
+export default {
+    props: ['pageCreated'],
+    computed: {
+        isFormInvalid() {
+            return !this.pageTitle || !this.content;
+        }
+    },
+    data() {
+        return {
+            pageTitle: '',
+            content: ''
+        }
+    },
+    methods: {
+        submitForm(){
+            if(!this.pageTitle || !this.content) {
+                alert('Please fill in the form.');
+                return;
+            }
+            this.pageCreated({
+                pageTitle: this.pageTitle,
+                content: this.content,
+
+            });
+        }
+    }
+}
+
+</script>
