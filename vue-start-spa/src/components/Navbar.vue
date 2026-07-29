0325f9ab39c97bbba4f7b7db76400d635f39d42a
James Dunstan <jdunstan355@gmail.com>
2026-07-29 20:36:52 +1000

    Added Mobile functionality to navbar in the form of dropdown menu

--- vue-start-spa/src/components/Navbar.vue
+++ vue-start-spa/src/components/Navbar.vue
@@ -1,24 +1,43 @@
 <template>
-    <div class="background">
-    <nav class="nav nav-pills justify-content-center py-3">
-            <router-link class="nav-link" to="/">Home</router-link> 
-            <router-link class="nav-link" to="/needHelp">Need Help?</router-link> 
-            <router-link class="nav-link" to="/getInvolved">Get Involved!</router-link> 
-            <router-link class="nav-link" to="/about">About</router-link>
-            <router-link class="nav-link" to="/location">Locations</router-link> 
-            <router-link class="nav-link" to="/donation">Make a Donation!</router-link> 
-            <router-link class="nav-link" to="/contact">Contact</router-link>
+    
 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" to="/">
+                Company Name
+            </router-link>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLinks">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse justify-content-center" id="navbarLinks">
+                <div class="navbar-nav">
+                     <router-link class="nav-link" to="/">Home</router-link> 
+                     <router-link class="nav-link" to="/needHelp">Need Help?</router-link> 
+                     <router-link class="nav-link" to="/getInvolved">Get Involved!</router-link> 
+                     <router-link class="nav-link" to="/about">About</router-link>
+                     <router-link class="nav-link" to="/location">Locations</router-link> 
+                     <router-link class="nav-link" to="/donation">Make a Donation!</router-link> 
+                     <router-link class="nav-link" to="/contact">Contact</router-link>
+                </div>
+            </div>
+
+        </div>
     </nav>
-    </div>
+
+
+
+
+
+    
+    
 
 
 </template>
 
 <style scoped>
 
-    div {
-        background-color: aquamarine;
-    }
+    
 
 </style>
--- vue-start-spa/src/main.js
+++ vue-start-spa/src/main.js
@@ -1,6 +1,8 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import router from "./router";
 
