0d6e677ad5da9635eb210dd619af965dfd0a0ce4
James Dunstan <jdunstan355@gmail.com>
2026-07-29 19:48:28 +1000

    Added Navbar component so I don't have to repeat stylings

--- vue-start-spa/src/App.vue
+++ vue-start-spa/src/App.vue
@@ -1,22 +1,27 @@
 <template>
 
     <div>
-        <nav>
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/contact">Contact</router-link> |
-            <router-link to="/needHelp">Need Help?</router-link> |
-            <router-link to="/getInvolved">Get Involved!</router-link> |
-            <router-link to="/donation">Make a Donation!</router-link> |
-            <router-link to="/location">Locations</router-link>
-        </nav>
+        <Navbar></Navbar>
 
-        <hr>
+        
 
-        <router-view></router-view>
+        
 
 
     </div>
-
+    <div>
+        <router-view></router-view>
+    </div>
 
 </template>
+
+<script>
+    import Navbar from './components/Navbar.vue';
+
+    export default {
+        components: {
+            Navbar
+        }
+    };
+
+</script>
---
+++ vue-start-spa/src/components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="background">
+    <nav class="nav nav-pills justify-content-center py-3">
+            <router-link class="nav-link" to="/">Home</router-link> 
+            <router-link class="nav-link" to="/needHelp">Need Help?</router-link> 
+            <router-link class="nav-link" to="/getInvolved">Get Involved!</router-link> 
+            <router-link class="nav-link" to="/about">About</router-link>
+            <router-link class="nav-link" to="/location">Locations</router-link> 
+            <router-link class="nav-link" to="/donation">Make a Donation!</router-link> 
+            <router-link class="nav-link" to="/contact">Contact</router-link>
+
+    </nav>
+    </div>
+
+
+</template>
+
+<style scoped>
+
+    div {
+        background-color: aquamarine;
+    }
+
+</style>
